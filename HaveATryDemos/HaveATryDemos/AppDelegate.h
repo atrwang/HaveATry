37efe0df255ee33b8029a8bf016bb914de03269a
kevin.wang <atrwang@gmail.com>
2014-07-21 21:14:39 +0800

    Add HaveATry Project Clone ASIHttpRequest

---
+++ HaveATryDemos/HaveATryDemos/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HaveATryDemos
+//
+//  Created by Kevin.Wang on 7/21/14.
+//  Copyright (c) 2014 plaso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HaveATryDemos/HaveATryDemos/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HaveATryDemos
+//
+//  Created by Kevin.Wang on 7/21/14.
+//  Copyright (c) 2014 plaso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
